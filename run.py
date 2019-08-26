1eba851b35308c1b5c29919dfe1474ea2d2f842b
David Moore <david192837@gmail.com>
2019-08-26 13:53:25 -0400

    run.py was reworked to only allow a set number (3, as of now) of simulator processes to run simultaneously. The main section of each of the execution loops was turned into a funciton to make the code a bit more modular.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import configparser
 import subprocess
 from shutil import copyfile
+import time
 
 #upperBound = 350
 #lowerBound = 250
@@ -9,6 +10,7 @@ from shutil import copyfile
 #paramList1 = [*range(lowerBound, upperBound+1, increment)]
 paramList1 = [x * 0.001 for x in range(1,11)]
 livingProcesses = []
+cntLivingProcesses = 0
 
 def read_config():
     config = configparser.ConfigParser()
@@ -24,6 +26,46 @@ def write_config(config):
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+def start_sim(files, ver, param1):
+    global cntLivingProcesses
+    global livingProcesses
+    
+    # Trap the process in a while loop if there are already enough 
+    # processes running
+    while cntLivingProcesses >= int(config['SETTINGS']['simultaneous processes']):
+        for process in livingProcesses:
+            if process.poll() != None:
+                livingProcesses.remove(process)
+                cntLivingProcesses -= 1
+                break
+        time.sleep(0.01)
+    
+    # Take off the .auto file extension
+    fileName, fileExt = os.path.splitext(files)
+    # Edit the profile for each parameter file
+    if ver == 1:
+        newPath = os.path.join(cwd,pathResults,runNum,fileName)
+    if ver == 2:
+        newPath = os.path.join(cwd,pathResults,runNum,fileName+'_'+str(param1))
+    os.makedirs(newPath)
+    with open(os.path.join(cwd,pathProfiles,files)) as f: 
+        profileString = f.read()
+    if ver == 2:
+        profileString = profileString.format(param1)
+    with open(os.path.join(newPath,"sim_cfg"),'w') as f:
+        f.write(profileString)
+    # Create a pid file which the c program will edit later
+    pid_file = open(os.path.join(newPath,'pid'), 'w')
+    pid_file.close()
+    # Print the system command for debug purposes
+    #print('sh -c "cd '+newPath+'; '+os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt" &')
+     
+    # Run the system command.
+    livingProcesses.append(subprocess.Popen([os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt'], cwd=newPath, shell=True))
+    cntLivingProcesses += 1
+    print("Process Launched")
+
+# Read the config file
 config = read_config()
 
 # Find the current working directory
@@ -35,7 +77,6 @@ runNum = config['SETTINGS']['run number']
 pathCode = config['SETTINGS']['program path']
 pathProfiles = config['SETTINGS']['profile path']
 pathResults = config['SETTINGS']['results path']
-genMultipleProfiles = int(config['SETTINGS']['auto generate profiles'])
 
 # Print the executable (code), profiles, and results directories
 #print(pathCode)
@@ -45,62 +86,21 @@ genMultipleProfiles = int(config['SETTINGS']['auto generate profiles'])
 # Attempt to compile the code
 os.system('cd '+pathCode+'; make eponsim; cd ..')
 
-if genMultipleProfiles == 0:
-    # For each profile in the profiles directory
-    for files in os.listdir(pathProfiles):
-        # If the file is an auto file (which the generic profile should be)
-        if files.endswith(".txt"):
-            # Take off the .txt file extension
-            fileName, fileExt = os.path.splitext(files)
-            # Make a new file name out of the run number (from the config file) and
-            # the profile name
-            newPath = os.path.join(cwd,pathResults,runNum+'_'+fileName)
-            os.mkdir(newPath)
-            # Copy the profile to its respective folder and rename it "sim_cfg"
-            copyfile(os.path.join(cwd,pathProfiles,files),os.path.join(newPath,"sim_cfg"))
-            # Create a pid file which the c program will edit later
-            pid_file = open(os.path.join(newPath,'pid'), 'w')
-            pid_file.close()
-            # Print the system command for debug purposes
-            #print('sh -c "cd '+newPath+'; '+os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt" &')
-             
-            # Run the system command.
-            #os.system('sh -c "cd '+newPath+'; '+os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt" &')
-            livingProcesses.append(subprocess.Popen([os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt'], cwd=newPath, shell=True))
-            print("run "+runNum+"_"+fileName+" started")
-    for process in livingProcesses:
-        process.wait()
-    print('All of the processes have completed.')
+# For each profile in the profiles directory
+for files in os.listdir(pathProfiles):
+    # If the file is a .txt file (which the profile should be)
+    if files.endswith(".txt"):
+        start_sim(files, 1, 0)
 
-# If genMultipleProfiles is ON (== 1), edit the base profile to have a range 
-# of options automatically
-if genMultipleProfiles == 1:
-    # For each profile in the profiles directory
-    for files in os.listdir(pathProfiles):
-        # If the file is a .auto file (which the auto gen parameters profiles should be)
-        if files.endswith(".auto"):
-            # Take off the .auto file extension
-            fileName, fileExt = os.path.splitext(files)
-            # Edit the profile for each parameter file
-            for param1 in paramList1:
-                newPath = os.path.join(cwd,pathResults,runNum,fileName+'_'+str(param1))
-                os.makedirs(newPath)
-                with open(os.path.join(cwd,pathProfiles,files)) as f: 
-                    profileString = f.read()
-                profileString = profileString.format(param1)
-                with open(os.path.join(newPath,"sim_cfg"),'w') as f:
-                    f.write(profileString)
-                # Create a pid file which the c program will edit later
-                pid_file = open(os.path.join(newPath,'pid'), 'w')
-                pid_file.close()
-                # Print the system command for debug purposes
-                #print('sh -c "cd '+newPath+'; '+os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt" &')
-                 
-                # Run the system command.
-                livingProcesses.append(subprocess.Popen([os.path.join(cwd,pathCode,'eponsim')+' > sim_log.txt'], cwd=newPath, shell=True))
-    for process in livingProcesses:
-        process.wait()
-    print('All of the processes have completed.')
+# Edit the base profile to have a range of options automatically
+# For each profile in the profiles directory
+for files in os.listdir(pathProfiles):
+    # If the file is a .auto file (which the auto gen parameters profiles should be)
+    if files.endswith(".auto"):
+        for param1 in paramList1:
+            start_sim(files, 2, param1)
+
+print('All Processes have Finished')
 
 # Rewrite the config file with the updated settings
 write_config(config)
